343774261b92bd942583e9e5c8a26fbe7e51ee9a
lkhuu96 <khuu96@gmail.com>
2022-05-18 12:38:52 -0700

    added starter files to project

---
+++ client/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Dania Furniture Product Page
+      </div>
+    )
+  }
+}
